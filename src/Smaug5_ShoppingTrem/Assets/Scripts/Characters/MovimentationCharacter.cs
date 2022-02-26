497df6947ba4400387eef8ba1d19333e4dee0e46
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-02-26 13:15:18 -0300

    Sistema de movimentação
    Melhorado o sistema de movimentação do personagem. Ainda não finalizado.

--- src/Smaug5_ShoppingTrem/Assets/Scripts/Characters/MovimentationCharacter.cs
+++ src/Smaug5_ShoppingTrem/Assets/Scripts/Characters/MovimentationCharacter.cs
@@ -2,73 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//ESSE COMPONENTE É ULTRAPASSADO, TODA A MOVIMENTAÇAO DO PERSONAGEM VAI SER MANIPULADO PELOS STATES MACHINES
 public class MovimentationCharacter : MonoBehaviour
 {   
     public float velOfMovimentation;
-    public float velOfMovHorizontal;
-    CharacterController charControll;
-    //public static float xMove;
-    public float multiplyGravityWhenFall;
-    private bool walkLeft, walkRight;
-    private float currentTimeOfMovimentation;
-    public float timeMaxForMovimentation;
+    public float velOfMovimentationTurn;
+    public float posXLine1, posXLine2, posXLine3;
+    private bool isTurningLeft, isTurningRight;
+    private float posXAlreadyAdded;
+    private CharacterController charControll;
+    private int currentLine;
 
-    private Animator animator;
- 
-    void Awake()
+    void Start()
     {
+        currentLine = 2;
         charControll = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
     }
 
     void Update ()
     {
-        //xMove = Input.GetAxis("Horizontal");
-        
-        /*
-        //gravidade
-        if (animator.GetInteger("animacaoTransicao") == 2 && !animator.GetBehaviour<JumpStateMachine>().pousando) 
-        {
-            gravity = 0;
-        } 
-        else if (animator.GetInteger("animacaoTransicao") == 2 && animator.GetBehaviour<JumpStateMachine>().pousando) 
-        { 
-            gravity = Physics.gravity.y * Time.deltaTime * multiplyGravityWhenFall; 
-        }
-        else 
-        { 
-            gravity = Physics.gravity.y * Time.deltaTime; 
-        }
-        */
+        charControll.Move(new Vector3(0, Physics.gravity.y, 0) * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow) & !walkLeft) {
+        //AQUI DEVE SER TRATADO A VELOCIDADE CRESCENTE AO DECORRER DO TEMPO NA FASE.
+        //
 
-            walkLeft = true;
-            walkRight = false;
+        if (isTurningLeft) {
+            Turning(-velOfMovimentationTurn);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) & !walkRight) {
-
-            walkRight = true;
-            walkLeft = false;
+        if (isTurningRight) {
+            Turning(velOfMovimentationTurn);
         }
 
-        if (walkLeft) {
-
-            if (currentTimeOfMovimentation <= timeMaxForMovimentation) {
-
-                charControll.Move(new Vector3(-velOfMovHorizontal, Physics.gravity.y, 0) * Time.deltaTime);
-                currentTimeOfMovimentation += Time.deltaTime;
-
-            } else { walkLeft = false; walkRight = false; currentTimeOfMovimentation = 0;}
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && currentLine != 1 && (!isTurningLeft || !isTurningRight)) {
+            isTurningLeft = true;
         }
-        if (walkRight) {
-            
-            if (currentTimeOfMovimentation <= timeMaxForMovimentation) {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && currentLine != 3 && (!isTurningLeft || !isTurningRight)) {
+            isTurningRight = true;
+        }
+    }
 
-                charControll.Move(new Vector3(velOfMovHorizontal, Physics.gravity.y, 0) * Time.deltaTime);
-                currentTimeOfMovimentation += Time.deltaTime;
+    private void Turning(float velToTurntionAdd) {
 
-            } else { walkRight = false; walkLeft = false; currentTimeOfMovimentation = 0;}
+        float currentValueToAdd = velToTurntionAdd * Time.deltaTime;
+        charControll.Move(new Vector3(currentValueToAdd, 0, 0));
+        posXAlreadyAdded += currentValueToAdd;
+
+        if (currentLine == 1 && transform.position.x + posXAlreadyAdded >= posXLine2) {
+            posXAlreadyAdded = 0;
+            currentLine = 2;
+            isTurningLeft = false;
+            isTurningRight = false;
+            transform.SetPositionAndRotation(new Vector3(posXLine2, 
+                transform.position.y, 
+                transform.position.z), 
+                transform.rotation);
+        }
+
+        if (currentLine == 2) {
+
+            if (transform.position.x + posXAlreadyAdded >= posXLine3) {
+                posXAlreadyAdded = 0;
+                currentLine = 3;
+                isTurningLeft = false;
+                isTurningRight = false;
+                transform.SetPositionAndRotation(new Vector3(posXLine3, 
+                    transform.position.y, 
+                    transform.position.z), 
+                    transform.rotation);
+            }
+            if (transform.position.x + posXAlreadyAdded <= posXLine1) {
+                posXAlreadyAdded = 0;
+                currentLine = 1;
+                isTurningLeft = false;
+                isTurningRight = false;
+                transform.position.Set(posXLine1, 0, 0);
+                transform.SetPositionAndRotation(new Vector3(posXLine1, 
+                    transform.position.y, 
+                    transform.position.z), 
+                    transform.rotation);
+            }
+        }
+
+        if (currentLine == 3 && transform.position.x + posXAlreadyAdded <= posXLine2) {
+            posXAlreadyAdded = 0;
+            currentLine = 2;
+            isTurningLeft = false;
+            isTurningRight = false;
+            transform.SetPositionAndRotation(new Vector3(posXLine2, 
+                transform.position.y, 
+                transform.position.z), 
+                transform.rotation);
         }
     }
 }
