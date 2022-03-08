18393537a4522c5e4db80d3ba3255e5a081f7b4e
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-03-08 11:10:02 -0300

    Interesse de compra dos passageiros
    criado o sistema de Interesse de compra dos passageiros, mas ainda não feito o sistema de compra e venda dele em sí.

---
+++ src/Smaug5_ShoppingTrem/Assets/Scripts/Passengers/DecideBuyInterest.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecideBuyInterest : MonoBehaviour
+{
+    public GameObject indicatorSalesPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inventory inventoryPLayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+
+        if (Random.value <= inventoryPLayer.chancePassengerInterestBuy) {
+
+            //está no banco do lado direito
+            if (transform.position.x > 0 ) {
+
+                Vector3 positionToSpawn = new Vector3( 
+                    transform.position.x - 2, 
+                    indicatorSalesPrefab.transform.position.y, 
+                    transform.position.z
+                );
+
+                Instantiate(indicatorSalesPrefab, positionToSpawn, 
+                    indicatorSalesPrefab.transform.rotation, transform);
+
+            //está no banco do lado esquerdo
+            } else {
+                Vector3 positionToSpawn = new Vector3( 
+                    transform.position.x + 2, 
+                    indicatorSalesPrefab.transform.position.y, 
+                    transform.position.z
+                );
+
+                Instantiate(indicatorSalesPrefab, positionToSpawn, 
+                    indicatorSalesPrefab.transform.rotation, transform);
+            }
+        }
+    }
+}
