9ec801f67d46d22dad53314007a9599f66d46383
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-06-05 18:11:01 -0300

    Animações da tela de loading
    adicionado as animações da tela de loading, e o material da caixa de candy que até o momento não está sendo utilizada, o material do fone e o choco não foi adicionado.

--- src/Smaug5_ShoppingTrem/Assets/Scripts/LoadingSceneGameplay/LoadingTrainGameplayAnim.cs
+++ src/Smaug5_ShoppingTrem/Assets/Scripts/LoadingSceneGameplay/LoadingTrainGameplayAnim.cs
@@ -2,26 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingTrainGameplayAnim : MonoBehaviour
 {
-    private float variableTest;
+    public float timeInLoading;
+    public GameObject loadingIconTxt;
+    public GameObject backgroundImg;
+    public Texture img1;
+    public Texture img2;
+    public float speedLoadingIconTxt;
+    public float loadingIconTxtMaxLeft;
+
+    private float currentTimeInLoad;
+    private float loadingIconTxtMaxRight;
+    private bool loadingIconTxtIsMovingLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        loadingIconTxtMaxRight = loadingIconTxt.transform.position.x;
+
+        if (Random.value > 0.5)
+        {
+            backgroundImg.GetComponent<UnityEngine.UI.RawImage>().texture = img1;
+        }
+        else
+        {
+            backgroundImg.GetComponent<UnityEngine.UI.RawImage>().texture = img2;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ao invés dessa verificação por contagem, vai ser por se a animation desse mesmo gameobject
-        //foi encerrada.
-        if (variableTest >= 300) {
-            SceneManager.LoadScene("TrainGameplay"); 
+
+        if (loadingIconTxtIsMovingLeft)
+        {
+            loadingIconTxt.transform.position = new Vector3(
+                loadingIconTxt.transform.position.x - (speedLoadingIconTxt * Time.deltaTime),
+                loadingIconTxt.transform.position.y,
+                loadingIconTxt.transform.position.z
+            );
+
+            if (loadingIconTxt.transform.position.x < loadingIconTxtMaxLeft)
+            {
+                loadingIconTxtIsMovingLeft = false;
+            }
         }
-        variableTest += 1;
-        //Debug.Log(variableTest);
+        else
+        {
+
+            loadingIconTxt.transform.position = new Vector3(
+                loadingIconTxt.transform.position.x + (speedLoadingIconTxt * Time.deltaTime),
+                loadingIconTxt.transform.position.y,
+                loadingIconTxt.transform.position.z
+            );
+
+            if (loadingIconTxt.transform.position.x > loadingIconTxtMaxRight)
+            {
+                loadingIconTxtIsMovingLeft = true;
+            }
+        }
+
+        if (currentTimeInLoad >= timeInLoading)
+        {
+            SceneManager.LoadScene("TrainGameplay");
+        }
+        currentTimeInLoad += Time.deltaTime;
     }
 }
