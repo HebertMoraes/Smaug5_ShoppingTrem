77285b87726294a6426a5d2537916345a74bcd73
Erick N <batistasd678@gmail.com>
2022-10-18 22:49:18 -0300

    Adicionadas novas telas de pop up

--- src/Smaug5_ShoppingTrem/Assets/Scripts/GlobalVariables/VariablesSave.cs
+++ src/Smaug5_ShoppingTrem/Assets/Scripts/GlobalVariables/VariablesSave.cs
@@ -7,9 +7,10 @@ using UnityEngine;
         //setar essa variável ao selecionar o produto desejado lá no menu
         public static allProductsToSell currentProductSelectedToSell;
         public static float countMoney = 10;
-        
+
         //esse countCash provavelmente não será usado pois só tem uma moeda no game até então
-        //public static int countCash;
+        public static int countCash;
+    
         public static int countCandy;
         public static int countChoco;
         public static int countFone;
