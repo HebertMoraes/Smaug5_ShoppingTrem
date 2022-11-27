7ff8ec68ccf9783785bf00457e456923d13010e8
Erick N <batistasd678@gmail.com>
2022-11-27 12:15:11 -0300

    adicionado popUp para notificar que não tem dinheiro

--- src/Smaug5_ShoppingTrem/Assets/Scripts/MainMenu/AddBuyProducts.cs
+++ src/Smaug5_ShoppingTrem/Assets/Scripts/MainMenu/AddBuyProducts.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class AddBuyProducts : MonoBehaviour
 {
-    public void BuyAddCandyX5() {
 
+    public GameObject notMoney;
+    public void BuyAddCandyX5() {
         if (VariablesSave.countMoney >= GameProductsBalanceVariables.buyPricePlayerCandy * 5) {
             VariablesSave.countMoney -= GameProductsBalanceVariables.buyPricePlayerCandy * 5;
             VariablesSave.countCandy += 5;
+        }
+        else
+        {
+            notMoney.SetActive(true);
         }
     }
     public void BuyAddChocoX5() {
@@ -17,6 +22,10 @@ public class AddBuyProducts : MonoBehaviour
             VariablesSave.countMoney -= GameProductsBalanceVariables.buyPricePlayerChoco * 5;
             VariablesSave.countChoco += 5;
         }
+        else
+        {
+            notMoney.SetActive(true);
+        }
     }
     public void BuyAddFoneX5() {
 
@@ -24,13 +33,20 @@ public class AddBuyProducts : MonoBehaviour
             VariablesSave.countMoney -= GameProductsBalanceVariables.buyPricePlayerFone * 5;
             VariablesSave.countFone += 5;
         }
+        else
+        {
+            notMoney.SetActive(true);
+        }
     }
-
     public void BuyAddCandyX10() {
 
         if (VariablesSave.countMoney >= GameProductsBalanceVariables.buyPricePlayerCandy * 10) {
             VariablesSave.countMoney -= GameProductsBalanceVariables.buyPricePlayerCandy * 10;
             VariablesSave.countCandy += 10;
+        }
+        else
+        {
+            notMoney.SetActive(true);
         }
     }
     public void BuyAddChocoX10() {
@@ -39,12 +55,20 @@ public class AddBuyProducts : MonoBehaviour
             VariablesSave.countMoney -= GameProductsBalanceVariables.buyPricePlayerChoco * 10;
             VariablesSave.countChoco += 10;
         }
+        else
+        {
+            notMoney.SetActive(true);
+        }
     }
     public void BuyAddFoneX10() {
 
         if (VariablesSave.countMoney >= GameProductsBalanceVariables.buyPricePlayerFone * 10) {
             VariablesSave.countMoney -= GameProductsBalanceVariables.buyPricePlayerFone * 10;
             VariablesSave.countFone += 10;
+        }
+        else
+        {
+            notMoney.SetActive(true);
         }
     }
 }
