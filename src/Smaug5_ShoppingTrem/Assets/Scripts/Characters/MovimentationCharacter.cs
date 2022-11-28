5ef3e958363e649731e89b55351582eaa6f2201f
Hebert Moraes <hebertmoraes2019@gmail.com>
2022-11-27 22:12:47 -0300

    Alterado movimentação para desktop
    mantido as duas versões do código em comentário

--- src/Smaug5_ShoppingTrem/Assets/Scripts/Characters/MovimentationCharacter.cs
+++ src/Smaug5_ShoppingTrem/Assets/Scripts/Characters/MovimentationCharacter.cs
@@ -33,104 +33,31 @@ public class MovimentationCharacter : MonoBehaviour
             charControll.Move(new Vector3(0, Physics.gravity.y, 0) * Time.deltaTime);
         }
 
-        // if (isTurningLeft) {
-            
-        //     if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && !alreadyMakeSecondTurn) {
-        //         if (currentLine == "mid") {
-
-        //             isTurningLeft = false;
-        //             isTurningRight = true;
-        //             currentLine = "left";
-
-        //             float totalLeft_Mid = System.Math.Abs(posXLineMid - posXLineLeft);
-        //             posXAlreadyIncremented = totalLeft_Mid - System.Math.Abs(posXAlreadyIncremented);
-
-        //             alreadyMakeSecondTurn = true;
-        //             Turning(velOfMovimentationLeftRight);
-        //             movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
-
-        //         } else if (currentLine == "right") {
-
-        //             isTurningLeft = false;
-        //             isTurningRight = true;
-        //             currentLine = "mid";
-                    
-        //             float totalMid_Right = posXLineRight - posXLineMid;
-        //             posXAlreadyIncremented = totalMid_Right - System.Math.Abs(posXAlreadyIncremented);
-
-        //             alreadyMakeSecondTurn = true;
-        //             Turning(velOfMovimentationLeftRight);
-        //             movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
-        //         }
-        //     } else {
-        //         Turning(-velOfMovimentationLeftRight);
-        //         movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
-        //     }
-        // }
-        // if (isTurningRight) {
-
-        //     if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && !alreadyMakeSecondTurn) {
-        //         if (currentLine == "left") {
-
-        //             isTurningRight = false;
-        //             isTurningLeft = true;
-        //             currentLine = "mid";
-                    
-        //             float totalLeft_Mid = System.Math.Abs(posXLineLeft - posXLineMid);
-        //             posXAlreadyIncremented = (totalLeft_Mid - posXAlreadyIncremented) * -1;
-
-        //             alreadyMakeSecondTurn = true;
-        //             Turning(-velOfMovimentationLeftRight);
-        //             movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
-
-        //         } else if (currentLine == "mid") {
-        //             isTurningRight = false;
-        //             isTurningLeft = true;
-        //             currentLine = "right";
-                    
-        //             float totalMid_Right = posXLineRight - posXLineMid;
-        //             posXAlreadyIncremented = (totalMid_Right - posXAlreadyIncremented) * -1;
-
-        //             alreadyMakeSecondTurn = true;
-        //             Turning(-velOfMovimentationLeftRight);
-        //             movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
-        //         }
-        //     } else {
-        //         Turning(velOfMovimentationLeftRight);
-        //         movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
-        //     }
-        // }
-
-        // if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && currentLine != "left") {
-        //     if (!isTurningLeft && !isTurningRight) {
-        //         isTurningLeft = true;
-        //     }
-        // }
-        // if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && currentLine != "right") {
-        //     if (!isTurningLeft && !isTurningRight) {
-        //         isTurningRight = true;
-        //     }
-        // }
-
         if (isTurningLeft) {
             
-            if (touchSceenControll.CheckSwipTouchToRight() && !alreadyMakeSecondTurn) {
+            if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && !alreadyMakeSecondTurn) {
                 if (currentLine == "mid") {
+
                     isTurningLeft = false;
                     isTurningRight = true;
                     currentLine = "left";
+
                     float totalLeft_Mid = System.Math.Abs(posXLineMid - posXLineLeft);
                     posXAlreadyIncremented = totalLeft_Mid - System.Math.Abs(posXAlreadyIncremented);
+
                     alreadyMakeSecondTurn = true;
                     Turning(velOfMovimentationLeftRight);
                     movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
+
                 } else if (currentLine == "right") {
+
                     isTurningLeft = false;
                     isTurningRight = true;
                     currentLine = "mid";
                     
                     float totalMid_Right = posXLineRight - posXLineMid;
                     posXAlreadyIncremented = totalMid_Right - System.Math.Abs(posXAlreadyIncremented);
+
                     alreadyMakeSecondTurn = true;
                     Turning(velOfMovimentationLeftRight);
                     movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
@@ -141,17 +68,21 @@ public class MovimentationCharacter : MonoBehaviour
             }
         }
         if (isTurningRight) {
-            if (touchSceenControll.CheckSwipTouchToLeft() && !alreadyMakeSecondTurn) {
+
+            if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && !alreadyMakeSecondTurn) {
                 if (currentLine == "left") {
+
                     isTurningRight = false;
                     isTurningLeft = true;
                     currentLine = "mid";
                     
                     float totalLeft_Mid = System.Math.Abs(posXLineLeft - posXLineMid);
                     posXAlreadyIncremented = (totalLeft_Mid - posXAlreadyIncremented) * -1;
+
                     alreadyMakeSecondTurn = true;
                     Turning(-velOfMovimentationLeftRight);
                     movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
+
                 } else if (currentLine == "mid") {
                     isTurningRight = false;
                     isTurningLeft = true;
@@ -159,6 +90,7 @@ public class MovimentationCharacter : MonoBehaviour
                     
                     float totalMid_Right = posXLineRight - posXLineMid;
                     posXAlreadyIncremented = (totalMid_Right - posXAlreadyIncremented) * -1;
+
                     alreadyMakeSecondTurn = true;
                     Turning(-velOfMovimentationLeftRight);
                     movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
@@ -168,16 +100,84 @@ public class MovimentationCharacter : MonoBehaviour
                 movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
             }
         }
-        if (touchSceenControll.CheckSwipTouchToLeft() && currentLine != "left") {
+
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && currentLine != "left") {
             if (!isTurningLeft && !isTurningRight) {
                 isTurningLeft = true;
             }
         }
-        if (touchSceenControll.CheckSwipTouchToRight() && currentLine != "right") {
+        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && currentLine != "right") {
             if (!isTurningLeft && !isTurningRight) {
                 isTurningRight = true;
             }
         }
+
+        // if (isTurningLeft) {
+            
+        //     if (touchSceenControll.CheckSwipTouchToRight() && !alreadyMakeSecondTurn) {
+        //         if (currentLine == "mid") {
+        //             isTurningLeft = false;
+        //             isTurningRight = true;
+        //             currentLine = "left";
+        //             float totalLeft_Mid = System.Math.Abs(posXLineMid - posXLineLeft);
+        //             posXAlreadyIncremented = totalLeft_Mid - System.Math.Abs(posXAlreadyIncremented);
+        //             alreadyMakeSecondTurn = true;
+        //             Turning(velOfMovimentationLeftRight);
+        //             movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
+        //         } else if (currentLine == "right") {
+        //             isTurningLeft = false;
+        //             isTurningRight = true;
+        //             currentLine = "mid";
+                    
+        //             float totalMid_Right = posXLineRight - posXLineMid;
+        //             posXAlreadyIncremented = totalMid_Right - System.Math.Abs(posXAlreadyIncremented);
+        //             alreadyMakeSecondTurn = true;
+        //             Turning(velOfMovimentationLeftRight);
+        //             movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
+        //         }
+        //     } else {
+        //         Turning(-velOfMovimentationLeftRight);
+        //         movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
+        //     }
+        // }
+        // if (isTurningRight) {
+        //     if (touchSceenControll.CheckSwipTouchToLeft() && !alreadyMakeSecondTurn) {
+        //         if (currentLine == "left") {
+        //             isTurningRight = false;
+        //             isTurningLeft = true;
+        //             currentLine = "mid";
+                    
+        //             float totalLeft_Mid = System.Math.Abs(posXLineLeft - posXLineMid);
+        //             posXAlreadyIncremented = (totalLeft_Mid - posXAlreadyIncremented) * -1;
+        //             alreadyMakeSecondTurn = true;
+        //             Turning(-velOfMovimentationLeftRight);
+        //             movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
+        //         } else if (currentLine == "mid") {
+        //             isTurningRight = false;
+        //             isTurningLeft = true;
+        //             currentLine = "right";
+                    
+        //             float totalMid_Right = posXLineRight - posXLineMid;
+        //             posXAlreadyIncremented = (totalMid_Right - posXAlreadyIncremented) * -1;
+        //             alreadyMakeSecondTurn = true;
+        //             Turning(-velOfMovimentationLeftRight);
+        //             movimentationGuard.TurningGuard(-velOfMovimentationLeftRight);
+        //         }
+        //     } else {
+        //         Turning(velOfMovimentationLeftRight);
+        //         movimentationGuard.TurningGuard(velOfMovimentationLeftRight);
+        //     }
+        // }
+        // if (touchSceenControll.CheckSwipTouchToLeft() && currentLine != "left") {
+        //     if (!isTurningLeft && !isTurningRight) {
+        //         isTurningLeft = true;
+        //     }
+        // }
+        // if (touchSceenControll.CheckSwipTouchToRight() && currentLine != "right") {
+        //     if (!isTurningLeft && !isTurningRight) {
+        //         isTurningRight = true;
+        //     }
+        // }
     }
 
     private void Turning(float velToTurnAdd) {
